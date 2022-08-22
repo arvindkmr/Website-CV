7de44eaf2d7aa360e93b8ddd344b80b77a2c204b
arvind <developer.arvind003@gmail.com>
2022-08-23 00:04:43 +0530

    fixed title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import photo from './photo.jpg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={photo}   className="App-logo" alt="logo" />
-        <p>
-          Arvind Baloda - Frontend (React ) Developer - SDE-1
-        </p>
+        <img src={photo} className="App-logo" alt="logo" />
+        <p>Arvind Baloda - Frontend (React ) Developer - SDE-1</p>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/arvind-baloda-%F0%9F%98%8A-b4b4421b2/"
-          // target="_blank"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Linkedin Profile
@@ -22,10 +19,10 @@ function App() {
         <a
           className="App-link"
           href="https://github.com/arvindkmr"
-          // target="_blank"
+          target="_blank"
           rel="noopener noreferrer"
         >
-Github profile
+          Github profile
         </a>
       </header>
     </div>
